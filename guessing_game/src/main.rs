1093810e5447a11e1167087aa948f4ec2be5fe54
Tongzhoutz <zhoutong.econ@gmail.com>
2025-08-29 08:42:26 -0400

    Finish guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,15 @@
+use std::cmp::Ordering;
 use std::io; // preclude: https://doc.rust-lang.org/stable/std/prelude/index.html
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
     
     let mut guess = String::new(); // mutable; new: associated function
@@ -11,5 +19,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     
+    let guess: u32 = guess.trim().parse().expect("please type a number!");
+    
     println!("You guessed: {guess}");
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small,!"),
+        Ordering::Greater => println!("Too large!"), 
+        Ordering::Equal => println!("You win!"),
+    }
 }
