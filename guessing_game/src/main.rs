db167a5520fd2cdfb2178f60d13b503bd43b420a
Tongzhoutz <zhoutong.econ@gmail.com>
2025-08-29 18:23:27 -0400

    Finish guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,25 +7,31 @@ fn main() {
     println!("Guess the number!");
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
     
-    let mut guess = String::new(); // mutable; new: associated function
+        let mut guess = String::new(); // mutable; new: associated function
                                    // created a mutable variable that is currently bound to a new, empty instance of a String. Whew!
     
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
     
-    let guess: u32 = guess.trim().parse().expect("please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
     
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
     
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small,!"),
-        Ordering::Greater => println!("Too large!"), 
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"), 
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
